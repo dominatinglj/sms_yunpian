bdf80f0d72656efebefacc148e2906960e8d7e95
jun_liao <junliao3344@gmail.com>
2015-10-23 11:24:36 +0800

    修改发送短信的

--- lib/sms_yunpian/request.rb
+++ lib/sms_yunpian/request.rb
@@ -12,12 +12,16 @@ module SmsYunpian
     uri              = URI.parse('http://yunpian.com/v1/sms/send.json')
     http             = Net::HTTP.new(uri.host,uri.port)
     params           = {}
-    params['apikey'] = @api_key #'c5d7d3b0c38e84fd03e438f4c8ef5989'
-    params['mobile'] = mobile#'18959203752'
-    params['text']   = text #'【代迹网】您的验证码是132467'
+    params['apikey'] = @api_key 
+    params['mobile'] = mobile
+    params['text']   = text 
     req              = Net::HTTP::Post.new(uri)
     req.set_form_data(params)
     response         = http.request(req)
     puts response.body
+  end
+  
+  def self.send_security_code(mobile,code)
+    self.send(mobile, "【代迹网】您的验证码是#{code}")
   end
 end
